704e892fe74065b47695a29575352dbc3db13973
kderou19 <kderousselle@student.neumont.edu>
2013-11-27 15:19:57 -0700

    finished CommentManger...tentatively....maybe....

--- Project/Project/src/managers/CommentManager.java
+++ Project/Project/src/managers/CommentManager.java
@@ -5,11 +5,30 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import entities.Comment;
+
 @Stateless
 @LocalBean
 public class CommentManager {
 	@PersistenceContext
 	EntityManager em;
 	
+	public Comment getComment(int commentID){
+		return em.find(Comment.class,commentID);
+	}
 	
+	public Comment createComment(Comment c){
+		em.persist(c);
+		return c;
+	}
+	
+	public Comment updateComment(Comment c){
+		em.merge(c);
+		return c;
+	}
+	
+	public void deleteComment(int commentID){
+		Comment c = getComment(commentID);
+		em.remove(c);
+	}
 }
