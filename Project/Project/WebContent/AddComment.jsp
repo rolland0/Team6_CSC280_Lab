c6f58c7a3c734782dd49d4fc2ec18bcc4622c3ac
Erica Bradford <ebradford@student.neumont.edu>
2013-12-02 15:22:57 -0700

    fixed/finished addComment

--- Project/Project/WebContent/AddComment.jsp
+++ Project/Project/WebContent/AddComment.jsp
@@ -1,7 +1,7 @@
 <!-- this should be a partial page that is added to the post page. -->
 
 <form method="post" action="AddComment">
-<input type="hidden" name="origPost" value=""/>
+<input type="hidden" name="origPost" value="${post.id }"/>
 <textarea rows="10" cols="50" name="comment">Add a comment</textarea>
 <input type="submit" value="Add comment">
 </form>
--- Project/Project/src/servlets/AddCommentServlet.java
+++ Project/Project/src/servlets/AddCommentServlet.java
@@ -9,10 +9,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import entities.Comment;
+import entities.Post;
 import entities.User;
 import managers.CommentManager;
+import managers.PostManager;
 import managers.UserManager;
 
 /**
@@ -26,26 +29,27 @@ public class AddCommentServlet extends HttpServlet {
 	CommentManager cm;
 	@EJB
 	UserManager um;
+	@EJB
+	PostManager pm;
 	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServletContext context = request.getServletContext();
+		HttpSession session = request.getSession();
 		
-		User user = (User) context.getAttribute("currentUser");
+		User user = (User) session.getAttribute("currentUser");
 		
 		String content = request.getParameter("comment");
+		int id = Integer.parseInt(request.getParameter("origPost"));
 		
+		Post thePost = pm.getPost(id);
 		Comment comment = new Comment();
 		
 		comment.setContent(content);
 		comment.setOriginalPoster(user);
-		//comment.setPost(origPost);
-		//comment.setReplies(replies);    ???
+		comment.setPost(thePost);
 		
-		//double check on this one
-		user.getComments().add(comment);
+		//comment.setReplies(replies); 
 		
-		um.update(user);
 		cm.createComment(comment);
 	}
 
