6425ecff1d36414fee0d378d7cd59fbdd73012e9
ecollopy <ecollopy@student.neumont.edu>
2013-12-04 14:35:20 -0700

    make view post actually use the database

--- Project/Project/src/servlets/ViewPost.java
+++ Project/Project/src/servlets/ViewPost.java
@@ -1,18 +1,27 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import entities.Post;
+import managers.PostManager;
+
 @WebServlet("/ViewPost")
 public class ViewPost extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@EJB
+	PostManager pm;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("post", request.getAttribute("id"));
+		int id = (int) request.getAttribute("id");
+		Post post = pm.getPost(id);
+		request.setAttribute("post", post);
 		request.getRequestDispatcher("displayPost.jsp").forward(request, response);
 	}
 }
