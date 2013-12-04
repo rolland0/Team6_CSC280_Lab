7b70a93668499e48f08864b355ed084d13ed0fe6
kderou19 <kderousselle@student.neumont.edu>
2013-12-04 15:48:29 -0700

    commented out sacurity for testing purposes

--- Project/Project/src/servlets/AddPostServlet.java
+++ Project/Project/src/servlets/AddPostServlet.java
@@ -26,7 +26,7 @@ public class AddPostServlet extends HttpServlet {
 	@EJB
 	UserManager um;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("WEB-INF/CRUDpage/AddPost.jsp").forward(request, response);;
+		request.getRequestDispatcher("WEB-INF/CRUDpage/AddPost.jsp").forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
