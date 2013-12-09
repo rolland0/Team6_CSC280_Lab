14958201749fabf93e6ad95fd0a03cb275bcbc3b
Chris <csimpson_account@hotmail.com>
2013-12-09 10:01:09 -0700

    Added searching for posts

---
+++ Project/Project/WebContent/WEB-INF/searchResults.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" href="style/Style.css" type="text/css">
+<title>Search Results</title>
+</head>
+<body>
+	<c:if test="${not empty requestScope.titleMatches}">
+		Title Matches <br>
+		<c:forEach items="${titleMatches}" var="titleMatch">
+		<c:set var="postLink" value="ViewPost?id=${titleMatch.id}" />
+			<a href="${postLink}">
+			<c:out value="${titleMatch.title }"></c:out>
+			</a><br />
+		</c:forEach>
+	</c:if>
+	<br>
+	
+	<c:if test="${not empty requestScope.contentMatches}">
+		Content Matches <br>
+		<c:forEach items="${contentMatches}" var="contentMatch">
+			<c:set var="postLink" value="ViewPost?id=${contentMatch.id}" />
+			<a href="${postLink}">
+			<c:out value="${contentMatch.content }"></c:out>
+			</a><br />
+		</c:forEach>
+	</c:if>
+	<br>
+	
+</body>
+</html>
--- Project/Project/src/servlets/Search.java
+++ Project/Project/src/servlets/Search.java
@@ -1,20 +1,47 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import managers.PostManager;
+import entities.Post;
+
 @WebServlet("/Search")
 public class Search extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	PostManager postManager;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String keywords = request.getParameter("searchQuery");
 		if(keywords == null || keywords.isEmpty()) {
-			
+			response.sendRedirect("Setup");
+			return;
 		}
+		
+		List<Post> posts = postManager.getPosts();
+		ArrayList<Post> titleMatches = new ArrayList<>();
+		ArrayList<Post> contentMatches = new ArrayList<>();
+		
+		for (Post post : posts) {
+			if(post.getTitle().contains(keywords)) {
+				titleMatches.add(post);
+			}
+			if(post.getContent().contains(keywords)) {
+				contentMatches.add(post);
+			}
+		}
+		request.setAttribute("titleMatches", titleMatches);
+		request.setAttribute("contentMatches", contentMatches);
+		request.getRequestDispatcher("WEB-INF/searchResults.jsp").forward(request, response);
 	}
 }
