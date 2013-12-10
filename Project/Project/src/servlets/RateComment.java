a0f3c60463e347e9f935eac24aa80f448400585d
kderou19 <kderousselle@student.neumont.edu>
2013-12-10 15:24:25 -0700

    attempt to fix comment rating

--- Project/Project/src/servlets/RateComment.java
+++ Project/Project/src/servlets/RateComment.java
@@ -27,12 +27,15 @@ public class RateComment extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
 		Comment comment = cm.getComment(id);
+		
 		if(request.getParameter("upvote") != null){
 			comment.incrementRating();
 		}
 		else if(request.getParameter("downvote") != null){
 			comment.decrementRating();
 		}
+		cm.updateComment(comment);
+		
 		request.setAttribute("post", comment.getPost());
 		request.getRequestDispatcher("displayPost.jsp").forward(request, response);
 	}
