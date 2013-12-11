8d6c910243ccc102ba5d714626cb000c7f11be17
ecollopy <ecollopy@student.neumont.edu>
2013-12-11 14:51:31 -0700

    admin post deleting finished

--- Project/Project/src/servlets/DeletePost.java
+++ Project/Project/src/servlets/DeletePost.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.eclipse.persistence.indirection.IndirectSet;
 import entities.User;
 import managers.PostManager;
 import managers.UserManager;
@@ -25,16 +24,12 @@ public class DeletePost extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String message;
 		String userName = request.getRemoteUser();
-		System.out.println(userName);
 		User currentUser = um.getUserByName(userName);
-		IndirectSet groups = (IndirectSet) currentUser.getGroups();
 		//this method needs to be called in order to create the groups set
 		//simply calling toString does not actually hit the database
-//		currentUser.getGroups().isEmpty();
-//		System.out.println(currentUser.getGroups());
-//		if(currentUser.getGroups().toString().contains("admins")){
-		if(groups.contains("admins")){
-			System.out.println("you are an admin");
+		currentUser.getGroups().isEmpty();
+		System.out.println(currentUser.getGroups());
+		if(currentUser.getGroups().toString().contains("admins")){
 			int id = Integer.parseInt(request.getParameter("id"));
 			pm.delete(id);
 			message = "Post successfully deleted";
