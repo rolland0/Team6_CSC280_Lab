bd59eb1d3f592567370708391fc270143dcba104
kderou19 <kderousselle@student.neumont.edu>
2013-12-12 13:19:17 -0700

    hooking up error pages with message

--- Project/Project/src/servlets/RateComment.java
+++ Project/Project/src/servlets/RateComment.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.persistence.exceptions.DatabaseException;
+
 import entities.Comment;
 import managers.CommentManager;
 import managers.UserManager;
@@ -27,15 +29,20 @@ public class RateComment extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
 		Comment comment = cm.getComment(id);
-		
+
 		if(request.getParameter("upvote") != null){
 			comment.incrementRating();
 		}
 		else if(request.getParameter("downvote") != null){
 			comment.decrementRating();
 		}
-		cm.updateComment(comment);
-		
+		try{
+			cm.updateComment(comment);
+		}catch(DatabaseException e)
+		{
+			request.setAttribute("error", "We're sorry we couldn't rate the comment at this time. Perhaps the comment or post doesn't exist any longer.");
+			request.getRequestDispatcher("WEB-INF/error.jsp").forward(request, response);
+		}
 		request.setAttribute("post", comment.getPost());
 		request.getRequestDispatcher("WEB-INF/displayPost.jsp").forward(request, response);
 	}
