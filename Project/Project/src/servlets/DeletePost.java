197a5aee35e36ac036e20b1b40a91e31793e2586
ecollopy <ecollopy@student.neumont.edu>
2013-12-16 13:35:27 -0700

    more merging funtimes

--- Project/Project/src/servlets/DeletePost.java
+++ Project/Project/src/servlets/DeletePost.java
@@ -24,36 +24,36 @@ import managers.UserManager;
 @WebServlet("/DeletePost")
 @DeclareRoles({"admins"})
 public class DeletePost extends HttpServlet {
- private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
- @EJB
- PostManager pm;
- @EJB
- UserManager um;
- @EJB
- CommentManager cm;
- protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-  String message = null;
-  String userName = request.getRemoteUser();
-  User currentUser = um.getUserByName(userName);
-  //this method needs to be called in order to create the groups set
-  //simply calling toString does not actually hit the database
-  currentUser.getGroups().isEmpty();
-  if(currentUser.getGroups().toString().contains("admins")){
-   int id = Integer.parseInt(request.getParameter("id"));
-   try{
-    pm.delete(id);
-    currentUser.getPosts().remove(pm.getPost(id));
-    message = "Post successfully deleted.";
-   }catch(DatabaseException | EJBException | NullPointerException e){
-    request.setAttribute("error", "The post cannot be deleted at this time. It could've been deleted already.");
-    request.getRequestDispatcher("WEB-INF/error.jsp").forward(request, response);
-   }
-  }
-  else
-   message = "You do not have permission to delete posts";
-  
-  request.setAttribute("message", message);
-  request.getRequestDispatcher("GetPosts").forward(request, response);
- }
+	@EJB
+	PostManager pm;
+	@EJB
+	UserManager um;
+	@EJB
+	CommentManager cm;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String message = null;
+		String userName = request.getRemoteUser();
+		User currentUser = um.getUserByName(userName);
+		//this method needs to be called in order to create the groups set
+		//simply calling toString does not actually hit the database
+		currentUser.getGroups().isEmpty();
+		if(currentUser.getGroups().toString().contains("admins")){
+			int id = Integer.parseInt(request.getParameter("id"));
+			try{
+				pm.delete(id);
+				currentUser.getPosts().remove(pm.getPost(id));
+				message = "Post successfully deleted.";
+			}catch(DatabaseException | EJBException | NullPointerException e){
+				request.setAttribute("error", "The post cannot be deleted at this time. It could've been deleted already.");
+				request.getRequestDispatcher("WEB-INF/error.jsp").forward(request, response);
+			}
+		}
+		else
+			message = "You do not have permission to delete posts";
+
+		request.setAttribute("message", message);
+		request.getRequestDispatcher("GetPosts").forward(request, response);
+	}
 }
