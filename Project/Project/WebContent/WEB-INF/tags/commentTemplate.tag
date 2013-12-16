4ed0e1de1494e45b3c7ad3ed649db4c96a930b9c
ecollopy <ecollopy@student.neumont.edu>
2013-12-16 13:27:30 -0700

    deleting comments works, I think

--- Project/Project/WebContent/WEB-INF/tags/commentTemplate.tag
+++ Project/Project/WebContent/WEB-INF/tags/commentTemplate.tag
@@ -19,6 +19,14 @@
 	</form>
 </div>
 
+<div>
+	<form method="post" action="DeleteComment">
+		<input type="hidden" name="comment" value="${comment.id }"></input>
+		<input type="hidden" name="poster" value="${comment.poster }"></input>
+		<button type="submit">Delete this comment</button>
+	</form>
+</div>
+
 <li>
 	<div class="info">
 		<div class="commenterInfo">
--- Project/Project/src/servlets/DeleteComment.java
+++ Project/Project/src/servlets/DeleteComment.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+
 import javax.annotation.security.DeclareRoles;
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
@@ -9,40 +10,54 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.persistence.exceptions.DatabaseException;
+
+import entities.Comment;
+import entities.Post;
 import entities.User;
+import managers.CommentManager;
 import managers.PostManager;
 import managers.UserManager;
 
-@WebServlet("/DeletePost")
-@DeclareRoles({"admins"})
-public class DeletePost extends HttpServlet {
+@WebServlet("/DeleteComment")
+@DeclareRoles({"members","admins"})
+public class DeleteComment extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@EJB
-	PostManager pm;
+	CommentManager cm;
 	@EJB
 	UserManager um;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String message = null;
+	@EJB
+	PostManager pm;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String userName = request.getRemoteUser();
 		User currentUser = um.getUserByName(userName);
+		int commentId = Integer.parseInt(request.getParameter("comment"));
+		Comment comment = cm.getComment(commentId);
+		User OP = comment.getPoster();
+
+		String message = null;
 		//this method needs to be called in order to create the groups set
 		//simply calling toString does not actually hit the database
 		currentUser.getGroups().isEmpty();
-		if(currentUser.getGroups().toString().contains("admins")){
-			int id = Integer.parseInt(request.getParameter("id"));
+		if(currentUser.getGroups().toString().contains("admins") || currentUser==OP){
 			try{
-				pm.delete(id);
-				currentUser.getPosts().remove(pm.getPost(id));
-				message = "Post successfully deleted.";
+				cm.deleteComment(commentId);
+				Post post = comment.getPost();
+				post.getComments().remove(comment);
+				pm.update(post);
+				OP.getComments().remove(comment);
+				um.update(currentUser);
+				message = "Comment successfully deleted";
 			}catch(DatabaseException | EJBException | NullPointerException e){
-				request.setAttribute("error", "The post cannot be deleted at this time. It could've been deleted already.");
+				request.setAttribute("error", "The comment cannot be deleted at this time. It could've been deleted already.");
 				request.getRequestDispatcher("WEB-INF/error.jsp").forward(request, response);
 			}
 		}
 		else
-			message = "You do not have permission to delete posts";
+			message = "You do not have permission to delete this comment";
 		
 		request.setAttribute("message", message);
 		request.getRequestDispatcher("GetPosts").forward(request, response);
